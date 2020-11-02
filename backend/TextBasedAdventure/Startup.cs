20ea8075edeb5cebe16adcccf420830843c9b441
element2562 <element25668@gmail.com>
2020-11-01 20:11:00 -0600

    really added cors stuff this time

--- backend/TextBasedAdventure/Startup.cs
+++ backend/TextBasedAdventure/Startup.cs
@@ -26,6 +26,12 @@ namespace TextBasedAdventure
                                 Configuration.GetConnectionString("DefaultConnection")));
             //Register dapper in scope    
             services.AddScoped<IDapperService, DapperService>();
+
+            services.AddCors(o => o.AddPolicy("TextBasedAdventure", builder => {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,8 @@ namespace TextBasedAdventure
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("TextBasedAdventure");
 
             app.UseEndpoints(endpoints =>
             {
